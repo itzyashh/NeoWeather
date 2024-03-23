1629958d4c724db2031fd5a0d2becc337b017a51
Yash Jadhav <yashjadhav1411@rocketmail.com>
2024-03-23 21:38:22 +0530

    add trapezoid

--- src/tabbar/elements/TabbarItem.tsx
+++ src/tabbar/elements/TabbarItem.tsx
@@ -2,16 +2,19 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MapIcon from '../icons/MapIcon'
 import ListIcon from '../icons/ListIcon'
+import TrapezoidBackground from './TrapezoidBackground'
 
 const TabbarItem = () => {
   return (
-    <View style={{flex:1,
+    <View style={{
+    flex:1,
     flexDirection:'row',
     alignItems:'center',
     paddingHorizontal:32,
     justifyContent:'space-between',
 }}>
         <MapIcon />
+        <TrapezoidBackground />
         <ListIcon />
     </View>
   )
